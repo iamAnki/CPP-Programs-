773f851336c381dd2ccf7a606102933bbe238cf2
SAMBIT SAHA <68856365+sambitos23@users.noreply.github.com>
2021-10-08 00:05:08 +0530

    Add files via upload

--- activity-selection problem.c
+++ activity-selection problem.c
@@ -1,3 +1,6 @@
+// Write a program to implement the activity-selection problem stated as follows: You are given in activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time. Example: Consider the following 6 activities (0, 1, 2, 3, 4, 5), start[] = {1, 3, 0, 5, 8, 5); finish[]=(2, 4, 6, 7, 9, 9): The maximum set of
+// activities that can be executed by a single person is (0, 1, 3, 4).
+
 #include <stdio.h>
 void max_act(int s_517[], int f_517[], int n)
 {
