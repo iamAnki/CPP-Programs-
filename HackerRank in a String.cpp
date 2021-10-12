ce2c2aad53b1b1617b84ffef0e602767c3738413
kookstaen <52824392+kookstaen@users.noreply.github.com>
2021-10-12 13:11:40 +0530

    Add files via upload

---
+++ HackerRank in a String.cpp
@@ -0,0 +1,78 @@
+/*
+Hackerrank: HackerRank in a String!
+https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+We say that a string contains the word hackerrank if a subsequence of its characters spell the word hackerrank.
+Remeber that a subsequence maintains the order of characters selected from a sequence.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string hackerrankInString(string s) {
+    int j=0;
+    string h="hackerrank";
+    for(int i=0;i<s.length();i++) {
+        if(s[i]==h[j])
+            j++;
+    }
+    if(j==h.length())
+        return "YES";
+    else
+        return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = hackerrankInString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
