1bac56e2677192287ff7785499aee7184493135e
Umair Khan <92311678+umair228@users.noreply.github.com>
2021-10-29 16:17:36 +0500

    Alphabet Declaration
    This program take input from user as character;
    ALPHABET
    AND Identify and print the result as;
    VOWEL
    OR
    CONSONENT
    *Using SWITCH-CASE Concept.

---
+++ VowelORConsonent.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout<<"****** CONSONENT OR VOWEL *******"<<endl;
+	char ch;
+	cout<<"Enter the alphabet : ";
+	cin>>ch;
+	switch(ch){
+		case 'a':
+		cout<<"The Alphabet entered is vowel."<<endl;
+		break;
+		case 'e':
+		cout<<"The Alphabet entered is vowel."<<endl;
+		break;
+		case 'i':
+		cout<<"The Alphabet entered is vowel."<<endl;
+		break;
+		case 'o':
+		cout<<"The Alphabet entered is vowel."<<endl;
+		break;
+		case 'u':
+		cout<<"The Alphabet entered is vowel."<<endl;
+		break;
+		default:
+		cout<<"The Alphabet entered is a consonent."<<endl;
+	}
+	
+}
