b895ced89609d4bbad667eed2429cb33d631dd3c
UTPAL KUMAR <68495524+UTPALKUMARR@users.noreply.github.com>
2021-10-31 18:31:09 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ bool isPrime(int n)
+{
+	if(n==1)
+		return false;
+
+	if(n==2 || n==3)
+		return true;
+
+	if(n%2==0 || n%3==0)
+		return false;
+
+	for(int i=5; i*i<=n; i=i+6)
+	{
+		if(n % i == 0 || n % (i + 2) == 0)
+			return false; 
+	}
+
+	return true;
+}
+
+int main() {
+    
+    	int n = 77;
+    	
+    	printf("%s", isPrime(n) ? "true" : "false");
+    	
+    	return 0;
+}
